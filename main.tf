be2cf2b54191fa29ca2b0dd85c517c805c8e2768
Vishwa Keshara <vishk1027@gmail.com>
2026-06-22 02:02:00 +0530

    Add internet gateway and route table configuration for VPC

--- main.tf
+++ main.tf
@@ -15,3 +15,26 @@ resource "aws_subnet" "sub2" {
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+    }
+}   
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.RT.id
+}
