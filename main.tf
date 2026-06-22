980e1783d3afe74a2b9278ce8b2b5eeed167a06c
Vishwa Keshara <vishk1027@gmail.com>
2026-06-23 02:17:47 +0530

    Add Application Load Balancer and target group configurations

--- main.tf
+++ main.tf
@@ -93,3 +93,58 @@ resource "aws_instance" "webserver2" {
   subnet_id = aws_subnet.sub2.id
   user_data = base64encode(file("userdata1.sh"))
 }
+
+#Create alb
+resource "aws_lb" "myalb" {
+  name               = "myalb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups    = [aws_security_group.webSg.id]
+  subnets            = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+
+  tags = {
+    Name = "web"
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "myTg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+
+  health_check {
+    path                = "/"
+    port                = "traffic-port"
+  }  
+}
+
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+  
+}
+
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+  
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+  
+}
+output "alb_dns_name" {
+  value = aws_lb.myalb.dns_name
+}
