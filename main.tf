1b406fd045ee15a4b89c89afc3cdb7c9b46c26c4
Vishwa Keshara <vishk1027@gmail.com>
2026-06-23 02:31:08 +0530

    Format code for consistency in main.tf and provider.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ resource "aws_vpc" "myvpc" {
 }
 
 resource "aws_subnet" "sub1" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sub2" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
@@ -26,8 +26,8 @@ resource "aws_route_table" "RT" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
-    }
-}   
+  }
+}
 
 resource "aws_route_table_association" "rta1" {
   subnet_id      = aws_subnet.sub1.id
@@ -66,7 +66,7 @@ resource "aws_security_group" "webSg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    
+
   }
   tags = {
     Name = "Web-sg"
@@ -79,19 +79,19 @@ resource "aws_s3_bucket" "example" {
 }
 
 resource "aws_instance" "webserver1" {
-  ami           = "ami-0f8a61b66d1accaee"
-  instance_type = "t2.micro"
+  ami                    = "ami-0f8a61b66d1accaee"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
-  subnet_id = aws_subnet.sub1.id
-  user_data = base64encode(file("userdata.sh"))
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file("userdata.sh"))
 }
 
 resource "aws_instance" "webserver2" {
-  ami           = "ami-0f8a61b66d1accaee"
-  instance_type = "t2.micro"
+  ami                    = "ami-0f8a61b66d1accaee"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
-  subnet_id = aws_subnet.sub2.id
-  user_data = base64encode(file("userdata1.sh"))
+  subnet_id              = aws_subnet.sub2.id
+  user_data              = base64encode(file("userdata1.sh"))
 }
 
 #Create alb
@@ -100,8 +100,8 @@ resource "aws_lb" "myalb" {
   internal           = false
   load_balancer_type = "application"
 
-  security_groups    = [aws_security_group.webSg.id]
-  subnets            = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+  security_groups = [aws_security_group.webSg.id]
+  subnets         = [aws_subnet.sub1.id, aws_subnet.sub2.id]
 
   tags = {
     Name = "web"
@@ -115,23 +115,23 @@ resource "aws_lb_target_group" "tg" {
   vpc_id   = aws_vpc.myvpc.id
 
   health_check {
-    path                = "/"
-    port                = "traffic-port"
-  }  
+    path = "/"
+    port = "traffic-port"
+  }
 }
 
 resource "aws_lb_target_group_attachment" "attach1" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.webserver1.id
   port             = 80
-  
+
 }
 
 resource "aws_lb_target_group_attachment" "attach2" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.webserver2.id
   port             = 80
-  
+
 }
 
 resource "aws_lb_listener" "listener" {
@@ -143,7 +143,7 @@ resource "aws_lb_listener" "listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
   }
-  
+
 }
 output "alb_dns_name" {
   value = aws_lb.myalb.dns_name
